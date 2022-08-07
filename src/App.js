cef0db6762abc69572288cf3587f2b91bd7778d7
CVSCharan <charan.cvs@gmail.com>
2022-08-07 18:01:06 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 col-md-6">
+          <div className="card-container d-flex flex-column justify-content-center p-3">
+            <div className="d-flex flex-row justify-content-center p-1">
+              <img
+                src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/vr-optics-img.png"
+                className="img1"
+                alt="img1"
+              />
+            </div>
+            <div className="d-flex flex-row justify-content-center p-1">
+              <div>
+                <h1 className="cardheading">Improved Optics</h1>
+                <p className="cardpara">
+                  A virtual reality headset is a head-mounted device that
+                  provides virtual reality for the wearer.
+                </p>
+                <button className="butn btn btn-dark">Read more</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6">
+          <div className="card-container d-flex flex-column justify-content-center p-3">
+            <div className="d-flex flex-row justify-content-center p-1 order-md-2">
+              <img
+                src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/vr-controllers-img.png"
+                className="img1"
+                alt="img2"
+              />
+            </div>
+            <div className="d-flex flex-row justify-content-center p-1 oeder-md-1">
+              <div>
+                <h1 className="cardheading">Two Touch Controllers</h1>
+                <p className="cardpara">
+                  Your Oculus Touch controllers are a pair of tracked
+                  controllers that give you a hand.
+                </p>
+                <button className="butn btn btn-dark">Read more</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
